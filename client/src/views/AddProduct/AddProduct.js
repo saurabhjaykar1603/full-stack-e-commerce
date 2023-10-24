f8f95c6ea02b5247b085844515ffd3fa001b39c1
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-10-24 15:59:31 +0530

    feat : all products add successfully

--- client/src/views/AddProduct/AddProduct.js
+++ client/src/views/AddProduct/AddProduct.js
@@ -1,13 +1,106 @@
-import React, { useState } from 'react'
-import axios from "axios"
+import React, { useState } from "react";
+import axios from "axios";
+import showToast from "crunchy-toast";
 function AddProduct() {
-    const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  const [productImage, setProductImage] = useState("");
+  const [brand, setBrand] = useState("");
+
+  const addProduct = async () => {
+    if (!name || !description || !productImage || !price || !brand) {
+      showToast("Please enter all feilds", "warning");
+    }
+    const product = {
+      name: name,
+      description: description,
+      price: price,
+      brand: brand,
+      productImage: productImage,
+    };
+    const response = await axios.post("/products", product);
+
+    showToast(response.data.message);
+  };
   return (
     <div>
-      <h1 className='text-center'>Add Products</h1>
+      <h1 className="text-center">Add Products</h1>
 
+      <div
+        className="container w-50  rounded shadow h-100  mt-5 py-5"
+        style={{ border: "1px solid red" }}
+      >
+        <form action="">
+          <div className="container ">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Product Name"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+          </div>
+          <div className="container mt-4">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Product Price"
+              value={price}
+              onChange={(e) => {
+                setPrice(e.target.value);
+              }}
+            />
+          </div>
+          <div className="container mt-4">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Product Description"
+              value={description}
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            />
+          </div>
+          <div className="container mt-4">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Product Image Url"
+              value={productImage}
+              onChange={(e) => {
+                setProductImage(e.target.value);
+              }}
+            />
+          </div>
+          <div className="container mt-4">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Brand"
+              value={brand}
+              onChange={(e) => {
+                setBrand(e.target.value);
+              }}
+            />
+          </div>
+
+          <div className="container mt-4 text-center">
+            <button
+              className="btn btn-warning fs-5 px-5 shadow"
+              type="button"
+              onClick={addProduct}
+            >
+              Add Product
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AddProduct
+export default AddProduct;
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -2,65 +2,50 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Home() {
-  const [products, setProducts] = useState([
-    {
-      name: "Dal Makhani & Punjabi Choley",
-      description: "Dal Makhani and Punjabi Choley Combo (600 gms)",
-      productImage:
-        "https://www.haldirams.com/media/catalog/product/cache/91c328e4e467a384bb2dca2d478e6436/d/a/dalkmakhanipunjabi.jpg",
-      brand: "Brand A",
-      price: "500",
-    },
-    {
-      name: "Dal Makhani & Punjabi Choley",
-      description: "Dal Makhani and Punjabi Choley Combo (600 gms)",
-      productImage:
-        "https://www.haldirams.com/media/catalog/product/cache/91c328e4e467a384bb2dca2d478e6436/d/a/dalkmakhanipunjabi.jpg",
-      brand: "Brand A",
-      price: "500",
-    },
-  ]);
+  const [products, setProducts] = useState([]);
 
   const loadProducts = async () => {
-    const response = await axios.get('/products');
-    setProducts(response?.data?.data)
-  } ;
+    const response = await axios.get("/products");
+    setProducts(response?.data?.data);
+  };
 
   useEffect(() => {
-loadProducts();
-  },[])
+    loadProducts();
+  }, []);
   return (
     <>
       <h1 className="text-center">Home Page</h1>
 
-      {products?.map((product, i) => {
-        const { name, description, productImage, brand, price } = product;
-        return (
-          <div key={i} className="card shadow" style={{ width: "18rem" }}>
-            <div className="card-body text-center">
-              <img
-                src={productImage}
-                alt=""
-                className="mx-auto d-block m-0"
-                style={{ height: "200px" }}
-              />
-              <p className="fw-bold m-0 fs-5"> {name}</p>
-              <p className="text-success m-0 mt-2"> {description} </p>
-              <p className="m-0 mt-2">Price : {price}/ rs</p>
-              <p className="m-0 mt-2">{brand}</p>
-              <div className="container mt-2 bg-warning py-1 w-75  rounded">
-                <a
-                  href="/product/:${_id}"
-                  className="m-0 mt-1 text-dark"
-                  target="_blank"
-                >
-                  View details
-                </a>
+      <div className="d-flex justify-content-evenly flex-wrap">
+        {products?.map((product, i) => {
+          const { name, description, productImage, brand, price } = product;
+          return (
+            <div key={i} className="card shadow" style={{ width: "18rem", border: "1px solid tomato" }}>
+              <div className="card-body text-center">
+                <img
+                  src={productImage}
+                  alt=""
+                  className="mx-auto d-block m-0"
+                  style={{ height: "200px" }}
+                />
+                <p className="fw-bold m-0 fs-5"> {name}</p>
+                <p className="text-success m-0 mt-2"> {description} </p>
+                <p className="m-0 mt-2">Price : {price}/ rs</p>
+                <p className="m-0 mt-2">{brand}</p>
+                <div className="container mt-2 bg-warning py-1 w-75  rounded">
+                  <a
+                    href="/product/:${_id}"
+                    className="m-0 mt-1 text-dark"
+                    target="_blank"
+                  >
+                    View details
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </>
   );
 }
